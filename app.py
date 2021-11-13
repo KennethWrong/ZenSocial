f332f103e6f2ce238cdb0f20ad0f83fcf75d4604
werunm <cankennethwong@gmail.com>
2021-11-13 14:28:46 -0500

    polished up the syntax

--- app.py
+++ app.py
@@ -6,9 +6,15 @@ from flask_cors import CORS #comment this on deployment
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 CORS(app) #comment this on deployment
 
+
+##This is only for deployment
+# @app.route('/')
+# def home():
+#     return send_from_directory(app.static_folder,'index.html')
+
 @app.route('/')
 def home():
-    return send_from_directory(app.static_folder,'index.html')
+    return 'Hello there.'
 
 if __name__ == '__main__':
     app.run(debug=True)
